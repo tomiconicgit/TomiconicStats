663dd019ad67c8d8744639bce2723e84f48ef55e
tomiconicgit <tomiconicx@icloud.com>
2026-01-21 15:00:21 +0000

    Update github.js

--- modules/github.js
+++ modules/github.js
@@ -1,39 +1,25 @@
-export let githubAuth = {
-  username: '',
-  token: ''
-};
+export let githubAuth = { username: '', token: '' };
 
 export async function testLogin(username, token) {
   const res = await fetch('https://api.github.com/user', {
     headers: { Authorization: `token ${token}` }
   });
-  if (res.ok) {
-    githubAuth.username = username;
-    githubAuth.token = token;
-    return true;
-  } else {
-    return false;
-  }
+  if (res.ok) { githubAuth.username = username; githubAuth.token = token; return true; }
+  else return false;
 }
 
-export async function pushFileToRepo(path, content, repo, branch='main', message='Update via admin') {
+export async function pushContent(repo, path, content, branch='main') {
   const url = `https://api.github.com/repos/${githubAuth.username}/${repo}/contents/${path}`;
   
-  // Get SHA if file exists
-  const getRes = await fetch(url, {
-    headers: { Authorization: `token ${githubAuth.token}` }
-  });
   let sha;
-  if (getRes.ok) {
-    const data = await getRes.json();
-    sha = data.sha;
-  }
+  const getRes = await fetch(url, { headers: { Authorization: `token ${githubAuth.token}` } });
+  if (getRes.ok) { sha = (await getRes.json()).sha; }
 
   const res = await fetch(url, {
     method: 'PUT',
     headers: { Authorization: `token ${githubAuth.token}`, 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      message,
+      message: 'Admin Studio Content Update',
       content: btoa(unescape(encodeURIComponent(JSON.stringify(content, null, 2)))),
       branch,
       sha
